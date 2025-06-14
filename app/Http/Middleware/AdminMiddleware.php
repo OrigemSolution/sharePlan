ca8a6396cd8fd558e3b3b10c99141797e8bbe29a
Alben-cmd <alenoghenabenjamin@gmail.com>
2025-06-14 14:27:10 +0100

    middleware

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -4,6 +4,8 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class AdminMiddleware
