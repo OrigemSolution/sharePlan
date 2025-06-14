f08ca7d807e1ad00e0aaa971e30bf61c24c8d81e
Alben-cmd <alenoghenabenjamin@gmail.com>
2025-06-14 10:53:24 +0100

    seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,7 +20,8 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         $this->call([
-            RoleTableSeeder::class
+            RoleTableSeeder::class,
+            UserTableSeeder::class,
         ]);
     }
 }
--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Role;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UserTableSeeder extends Seeder
 {
@@ -16,10 +18,14 @@ class UserTableSeeder extends Seeder
         User::create([
             'name' => 'Admin User',
             'whatsapp_phone' => '07012343212',
+            'phone' => '07012343212',
             'email' => 'adminuser@gmail.com',
             'bank' => 'Admin Bank',
             'account_name' => 'Admin User',
             'account_no' => '43215678901',
+            'status' => 'verified',
+            'email_verified_at' => now(),
+            'role_id' => 2,
             'password' => Hash::make('password'),
             'created_at' => now(),
             'updated_at' => now(),
@@ -28,10 +34,14 @@ class UserTableSeeder extends Seeder
         User::create([
             'name' => 'User One',
             'whatsapp_phone' => '0701234333',
+            'phone' => '0701234333',
             'email' => 'userone@gmail.com',
             'bank' => 'User Bank',
             'account_name' => 'User One',
             'account_no' => '43232678901',
+            'role_id' => 1,
+            'status' => 'verified',
+            'email_verified_at' => now(),
             'password' => Hash::make('password'),
             'created_at' => now(),
             'updated_at' => now(),
